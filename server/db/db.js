cc7d56638767b38287e4941bfeccac79eef30c42
Bppatkar <bhanupratappatkar777@gmail.com>
2025-05-04 17:54:30 +0530

    first commit

---
+++ server/db/db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    const connection = await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "hospital_management", // Change the database name to "hospital_management"
+    });
+    console.log("Database Connected Successfully");
+    console.log(`MongoDB URI: ${process.env.MONGO_URI}`); // Log the MongoDB URI for debugging
+  } catch (error) {
+    console.log("Error in Database Connection: ", error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
