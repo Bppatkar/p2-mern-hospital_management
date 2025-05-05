86e2c48c14def233f93b6677c4b8caeda0af670e
Bppatkar <bhanupratappatkar777@gmail.com>
2025-05-05 14:53:59 +0530

    basic structure is done

---
+++ client/src/components/Appointment/UpdateAppointment.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UpdateAppointment = () => {
+  return <div>UpdateAppointment</div>;
+};
+
+export default UpdateAppointment;
---
+++ client/src/components/Doctor/UpdateDoctor.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UpdateDoctor = () => {
+  return <div>UpdateDoctor</div>;
+};
+
+export default UpdateDoctor;
